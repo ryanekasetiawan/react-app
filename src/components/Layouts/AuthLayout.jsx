0ecb0deab78e03d7d6bb38863cb4148315c4ce83
ryanekasetiawan <setiawanryaneka@gmail.com>
2024-07-29 09:56:57 +0700

    Conditional Rendering

--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const AuthLayout = (props) => {
-  const {children, title} = props;
+  const {children, title, type} = props;
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="w-full max-w-xs">
@@ -8,9 +10,32 @@ const AuthLayout = (props) => {
           Welcome, please enter your details
         </p>
         {children}
+        <Navigation type={type} />
       </div>  
     </div>
   );
+};
+
+const Navigation = ({type}) => {
+  if (type === 'login') {
+    return (
+      <p className="text-sm mt-5 text-center">
+        Don't have an account?{" "}
+        <Link to="/register" className="font-bold text-blue-600">
+          Register
+        </Link>
+      </p>
+    );  
+  } else {
+    return (
+      <p className="text-sm mt-5 text-center">
+        Already have an account?{" "}
+        <Link to="/login" className="font-bold text-blue-600">
+          Login
+        </Link>
+      </p>  
+    );
+  }
 };
 
 export default AuthLayout;
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,18 +1,11 @@
 import AuthLayout from "../components/Layouts/AuthLayout";
 import FormRegister from "../components/Fragments/FormRegister";
-import { Link } from "react-router-dom";
 
 
 const RegisterPage = () => {
   return (
-    <AuthLayout title="Register">
+    <AuthLayout title="Register" tipe="register">
       <FormRegister/>
-      <p className="text-sm mt-5 text-center">
-        Have an account?{" "}
-        <Link to="/login" className="font-bold text-blue-600">
-          Login
-        </Link>
-      </p>
     </AuthLayout>
   );
 };
