10b5815696234b62fcc2654e13203b504ac45835
ryanekasetiawan <setiawanryaneka@gmail.com>
2024-07-29 09:57:24 +0700

    Conditional Rendering

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,6 @@
 import AuthLayout from "../components/Layouts/AuthLayout";
 import FormRegister from "../components/Fragments/FormRegister";
 
-
 const RegisterPage = () => {
   return (
     <AuthLayout title="Register" tipe="register">
