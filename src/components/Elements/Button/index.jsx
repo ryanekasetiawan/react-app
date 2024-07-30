378f5dd71c2051ce30979c9ca547e99873deea45
ryanekasetiawan <setiawanryaneka@gmail.com>
2024-07-31 01:29:36 +0700

    Use State

--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
   const {classname="bg-black", children="", onClick = () => {}, type="button"} = props
   return (
-    <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} type="submit" onClick={() => onClick()}>
+    <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} type="submit" onClick={onClick}>
         {children}
       </button>
     );
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,28 +1,27 @@
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import Button from "../components/Elements/Button/";
 import CardProduct from "../components/Fragments/CardProduct";
-import Counter from "../components/Fragments/Counter";
 
 const products = [
   {
     id: 1,
     name: "Sepatu Baru",
     image: "/images/shoes-1.jpg",
-    price: "Rp 1.000.000",
+    price: 1000000,
     description: "exercitation eu reprehenderit laboris non velit consectetur proident aute mollit aute enim tempor sunt irure mollit ipsum proident voluptate ullamco"
   },
   {
     id: 2,
     name: "Sepatu Lama",
     image: "/images/shoes-1.jpg",
-    price: "Rp 500.000",
+    price: 500000,
     description: "exercitation eu reprehenderit laboris non velit"
   },
   {
     id: 3,
     name: "Sepatu Lama",
     image: "/images/shoes-1.jpg",
-    price: "Rp 500.000",
+    price: 2000000,
     description: "exercitation eu reprehenderit laboris non velit"
   }
 ];
@@ -30,6 +29,12 @@ const products = [
 const email = localStorage.getItem("email");
 
 const ProductPage = () => {
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1,
+    }
+  ]);
   
   const handleLogout = () => {
     localStorage.removeItem("email");
@@ -37,27 +42,65 @@ const ProductPage = () => {
     window.location.href = "/login";
   };
   
+  const handleAddToCart = (id) => {
+    setCart([
+      ...cart,
+      {
+        id,
+        qty: 1,
+      }
+    ]);
+  };
+  
   return (
     <Fragment>
-      <div className="flex justify-end h-16 md:h-20 bg-blue-600 text-white items-center px-2 md:px-10">
+      <div className="flex max-w-full justify-end h-20 bg-blue-600 text-white items-center px-10">
         {email}
         <Button classname="ml-5 bg-black" onClick={handleLogout}>Logout</Button>
       </div>
       <div className="flex justify-center py-5">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mr-4">
+        <div className="w-4/6 flex flex-wrap">
           {products.map((product) => (
             <CardProduct key={product.id}>
               <CardProduct.Header image={product.image} />
               <CardProduct.Body name={product.name}>
                 {product.description}
               </CardProduct.Body>
-              <CardProduct.Footer price={product.price} />
+              <CardProduct.Footer 
+              price={product.price}
+              id={product.id}
+              handleAddToCart={handleAddToCart}
+              />
             </CardProduct>
           ))}
         </div>
-      </div>
-      <div className="flex w-100 justify-center py-5">
-        <Counter></Counter>
+        <div className="w-2/6">
+          <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
+          <table className="text-left table-auto border-separate border-spacing-x-5">
+            <thead>
+              <tr>
+                <th>Product</th>  
+                <th>Price</th>  
+                <th>Quantity</th>  
+                <th>Total</th>  
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => {
+                const product = products.find(
+                    (product) => product.id === item.id
+                  );
+                  return (
+                    <tr key={item.id}>    <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>{item.qty}</td>
+                      <td>{item.qty * product.price}</td>
+                    </tr>
+                  );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </Fragment>
   );
