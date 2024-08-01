0b3849cb5532a6c7f7738f969adee71768b57dcc
ryanekasetiawan <setiawanryaneka@gmail.com>
2024-08-01 22:04:58 +0700

    Custom Hooks

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 import ErrorPage from "./pages/404";
 import ProductPage from "./pages/products";
+import ProfilePage from "./pages/profile";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
   {
     path: "/products",
     element: <ProductPage />
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />
   }
 ]);
 
